415086f92c3daaa2ff35dd72b5fd06ea6436a21d
mohit <mohit.cs2016@gmail.com>
2020-10-19 18:38:41 -0700

    all changes file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include("header.php"); ?>
+<?php include("navigation.php"); ?>
+<?php include("carousel.php");?>
+<?php include("aboutus.php"); ?>
+<?php include("services.php"); ?>
+<?php include ("portfolio.php"); ?>
+   <?php include ("clints.php"); ?>
+   <?php include ("testimonial.php");?>
+   <?php include("team.php");?>
+    <?php include("contactUs.php"); ?>
+	<?php include ("footer.php"); ?>
+    <?php include ("script.php"); ?>
+  
+
+
